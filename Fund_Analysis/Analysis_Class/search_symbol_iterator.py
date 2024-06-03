1cfa26358fc78e2db02e93233ea5492631c84478
blesswenyeyu <blesssecret@outlook.com>
2024-06-03 02:14:56 -0700

    add search symbol iterator

---
+++ Fund_Analysis/Analysis_Class/search_symbol_iterator.py
@@ -0,0 +1,33 @@
+import os,sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import investpy
+import datetime
+import common
+from airflow.exceptions import AirflowException
+from pathlib import Path
+home = str(Path.home())
+import pytz
+
+def main(start_symbol, end_symbol, input_file_path):
+    # iterator from [start_symbol to end_symbol]
+    # read Background.csv column 基金类型
+    # 货币型(test_2_money_market)，债券型(test_3_bonds)， 指数型 - 海外股票 / QDII(test_4_overseas), else (test.py)
+    for i in range(start_symbol, (end_symbol+1)):
+        formatted_number = f"{i:06}"  # Formats the number as a string with leading zeros up to 6 digits
+        # print(formatted_number)
+        symbol_file_path = input_file_path + "/" + formatted_number
+        print symbol_file_path
+    pass
+
+
+if __name__ == '__main__':
+    input_file_path = home + '/Desktop/output_china'
+    # output_file_path = home + '/Desktop/output_china'
+    try:
+        start_symbol = int(sys.argv[1])
+        end_symbol = int(sys.argv[2])
+        main(start_symbol, end_symbol, input_file_path)
+    except Exception as e:
+        raise AirflowException("fail to run at error ", e)
