7105d06dd74a68dac257b836e6892593edb1d0a5
GJGao <zg682@nyu.edu>
2024-06-19 00:48:36 -0400

    fix min vol typo

--- server/portfolio_opt/serializers.py
+++ server/portfolio_opt/serializers.py
@@ -4,7 +4,7 @@ class OptimizationInputSerializer(serializers.Serializer):
     symbols = serializers.ListField(child=serializers.CharField())
     amount = serializers.FloatField()
     method = serializers.ChoiceField(choices=[('equally_weighted', 'Equally Weighted'),
-                                              ('minimal_volatility', 'Minimal Volatility'),
+                                              ('min_volatility', 'Minimal Volatility'),
                                               ('max_sharpe_ratio', 'Max Sharpe Ratio')])
 
 class OptimizedPortfolioSerializer(serializers.Serializer):
