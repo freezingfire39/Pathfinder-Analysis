dd80a70df8b931614949e1c9ff5458c83290a8b5
freezingfire39 <yilunton@usc.edu>
2024-07-16 20:25:01 +0800

    Update test_2_money_market.py

--- Fund_Analysis/Analysis_Class/test_2_money_market.py
+++ Fund_Analysis/Analysis_Class/test_2_money_market.py
@@ -155,6 +155,8 @@ def main(symbol_file_path,symbol,search_file_path):
 
     df_target_2['benchmark_name']=0
     df_target_2.at[df_target_2.index[-1],'benchmark_name']  = "货币基金平均收益"
+    df_target_2['fund_name']=0
+    df_target_2.at[df_target_2.index[-1],'fund_name']  = str(df_background['基金简称'][0])
 
     #df_target['fee_gap'] = df_target['net_return']-df_target['return']
     df_target_2 = Analysis_class.return_analysis(df_target_2,input_file_path = symbol_file_path,rank_file_path = search_file_path+asset_type, asset_type=asset_type)
