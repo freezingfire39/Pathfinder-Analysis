1b1f65a10afba59a61a852d614317cbcc88690a9
freezingfire39 <yilunton@usc.edu>
2024-07-26 21:17:00 +0800

    Update test.py

--- Fund_Analysis/Analysis_Class/test.py
+++ Fund_Analysis/Analysis_Class/test.py
@@ -231,7 +231,7 @@ def main(symbol_file_path,symbol,search_file_path):
     Analysis_class.create_interesting_times_tear_sheet(df_target['return'])
     Analysis_class.create_interesting_times_tear_sheet(df_target['return'], benchmark_rets=df_target['comp_1'].pct_change())
 
-
+    df_target = Analysis_class.return_forecast(df_target, index_comps[comp_1_name],asset_type=asset_type)
 
 
     df_target.to_csv(save_file_path)
