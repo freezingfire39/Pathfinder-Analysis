bd33a10fca2291e71ed7d0e558cc5fc568545d26
freezingfire39 <yilunton@usc.edu>
2024-08-04 11:38:55 +0800

    Update test.py

--- Fund_Analysis/Analysis_Class/test.py
+++ Fund_Analysis/Analysis_Class/test.py
@@ -100,7 +100,7 @@ def main(symbol_file_path,symbol,search_file_path):
 
         close_days = df_test_1.index[-1]-df_test_2.index[-1]
 
-        df_target.at[df_target.index[-1],'purchase_days']  = "本基金距离上次开放赎回已经过去了"+close_days+"天。"
+        df_target.at[df_target.index[-1],'purchase_days']  = "本基金距离上次开放赎回已经过去了"+str(close_days)+"天。"
         df_test_2 = df_test_2.to_frame()
         df_test_2['flag']=1
         df_test_5 = df_test_2['flag'].resample('Y').sum()
