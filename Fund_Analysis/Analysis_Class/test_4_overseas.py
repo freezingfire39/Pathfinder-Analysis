b6be79d0cdb7f63f10e8652945683bb54f642153
freezingfire39 <yilunton@usc.edu>
2024-09-10 21:33:22 +0800

    Update test_4_overseas.py

--- Fund_Analysis/Analysis_Class/test_4_overseas.py
+++ Fund_Analysis/Analysis_Class/test_4_overseas.py
@@ -221,6 +221,8 @@ def main(symbol_file_path,symbol,search_file_path):
 
     df_target = Analysis_class.return_forecast(df_target, index_comps,asset_type=asset_type)
 
+    df_target.replace([np.inf, -np.inf], np.nan, inplace=True)
+    df_target.fillna(method='ffill',inplace=True)
 
     df_target.to_csv(save_file_path)
     #Analysis_class.rolling_volatility(df_target, index_comps[comp_1_name])
