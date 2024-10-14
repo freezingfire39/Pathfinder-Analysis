b8653919afe169bbaef22e040a60594fe6e52177
freezingfire39 <yilunton@usc.edu>
2024-10-14 22:16:10 +0800

    Update test_4_overseas.py

--- Fund_Analysis/Analysis_Class/test_4_overseas.py
+++ Fund_Analysis/Analysis_Class/test_4_overseas.py
@@ -220,13 +220,14 @@ def main(symbol_file_path,symbol,search_file_path):
 
 
     df_target = Analysis_class.plot_drawdown_underwater(df_target)
+    df_target.replace([np.inf, -np.inf], np.nan, inplace=True)
+    df_target.fillna(method='ffill',inplace=True)
+
 
     Analysis_class.event_analysis(df_target['return'], benchmark_rets=df_target['comp_1'].pct_change())
 
     df_target = Analysis_class.return_forecast(df_target, index_comps,asset_type=asset_type)
 
-    df_target.replace([np.inf, -np.inf], np.nan, inplace=True)
-    df_target.fillna(method='ffill',inplace=True)
 
     df_target.to_csv(save_file_path)
     #Analysis_class.rolling_volatility(df_target, index_comps[comp_1_name])
