133215f1b0cffe52701dba7505967c7379a6866e
freezingfire39 <yilunton@usc.edu>
2025-02-16 13:43:26 +0800

    Add files via upload

--- mutual_fund_timing/index_flow_7.py
+++ mutual_fund_timing/index_flow_7.py
@@ -14,17 +14,15 @@ print (df.head(20))
 
 
 
+
 #### import os, sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 #import investpy
-import yfinance as yf
-import pywt
 
 
-
-df['gap'] = (df['buy_elg_amount']/df['buy_lg_amount'])
+df['gap'] = (df['buy_lg_amount_rate']-df['buy_md_amount_rate'])
 
 #df['gap'] = df['gap'].cumsum()
 
@@ -34,41 +32,25 @@ df['gap'].plot()
 df['zscore'] = (df['gap'] - df['gap'].rolling(10).mean())/df['gap'].rolling(10).std(ddof=0)
 
 
-
-
 df['trade'] = 0
 df['trade'] = df['trade'].astype('float64')
 for i in range(len(df)):
     if i<1:
         continue
-        #if df_return['zscore'][i-1]>=1.2:
-        #    df_return['trade'][i]=-1
-    #if copper_future_2['return'][i-1]<-0.03:
-    #    copper_future_2['trade'][i:i+2]=1
-    
-    #elif copper_future_2['zscore'][i-1]>=1.3:
-    #    copper_future_2['trade'][i:i+3]=-1
-    
-    if df['zscore'][i-1]<=-0.8 and df['zscore'][i-1]>=-2.6:
-        df['trade'][i:i+1]=1
-    
 
-    elif df['zscore'][i-1]>=0.8 and df['zscore'][i-1]<=2.6:
+    
+    if df['zscore'][i-1]<=-0.7 and df['zscore'][i-1]>=-2.6:
         df['trade'][i:i+1]=-1
-    elif df['zscore'][i-1]<=0.8 and df['zscore'][i-1]>=0.:
-        df['trade'][i:i+1]=1
     
 
-    elif df['zscore'][i-1]>=-0.8 and df['zscore'][i-1]<=-0.:
+    elif df['zscore'][i-1]>=0.7 and df['zscore'][i-1]<=2.6:
+        df['trade'][i:i+1]=1
+    elif df['zscore'][i-1]<=0.7 and df['zscore'][i-1]>=0.2:
         df['trade'][i:i+1]=-1
+    
 
-
-
-    #elif copper_future_2['zscore'][i-1]<=0.2 and copper_future_2['zscore'][i-1]>=-0.2:
-    #    copper_future_2['trade'][i:]=0
-
-
-
+    elif df['zscore'][i-1]>=-0.7 and df['zscore'][i-1]<=-0.2:
+        df['trade'][i:i+1]=1
 
 
 df['full_return'] = df['trade']*(df['close_sh'].pct_change())
