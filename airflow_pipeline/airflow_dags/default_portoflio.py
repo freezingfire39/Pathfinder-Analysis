44592da421fef8e08276ee3e09243a8239949a0f
GJGao <zg682@nyu.edu>
2025-03-17 02:24:17 -0400

    add startdate

--- airflow_pipeline/airflow_dags/default_portoflio.py
+++ airflow_pipeline/airflow_dags/default_portoflio.py
@@ -1,8 +1,9 @@
+from datetime import datetime
+import pendulum
 from airflow import DAG
 from airflow.decorators import task
 
-DATA_DATE = '{{ data_interval_end }}'
-DJANGO_URL = 'localhost:8000'
+DJANGO_URL = '127.0.0.1:8000'
 BODY = {"weights":[{"symbol":"000552", "weight": 0.35},
                         {"symbol":"000415", "weight":0.35},
                         {"symbol":"001309", "weight":0.15},
@@ -15,16 +16,18 @@ default_args = {
     'depends_on_past': False,
     'retries': 3
 }
+local_tz = pendulum.timezone("America/New_York")# New York timezone
 
-@task(task_id='pull', templates_dict={"date": DATA_DATE, "url": DJANGO_URL, "body": BODY, "output": OUTPUT_FOLDER})
+@task(task_id='pull', templates_dict={ "url": DJANGO_URL, "body": BODY, "output": OUTPUT_FOLDER})
 def pull_result(**kwargs):
-    date = kwargs['date']
-    url = kwargs['url']
-    body = kwargs['body']
-    output = kwargs['output']
     import requests
     import json
     import os
+    import datetime
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    url = kwargs['url']
+    body = kwargs['body']
+    output = kwargs['output']
     full_url = url + "/api/v1/py/custom/portfolio/"
     response = requests.post(full_url, json=body)
     response.raise_for_status()
@@ -36,6 +39,8 @@ def pull_result(**kwargs):
 with DAG(
     dag_id='default_portoflio',
     schedule="0 10 * * 1-5" ,
+    start_date=datetime(2020, 1, 1, tzinfo=local_tz),
+    catchup=False,
     default_args=default_args,
 ) as dag:
     pull_default = pull_result()
