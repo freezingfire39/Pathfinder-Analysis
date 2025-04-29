82de5bf71223c0563083d2d62e2c8ba387f18063
freezingfire39 <yilunton@usc.edu>
2025-04-29 19:51:01 +0800

    Update file_list_sort.py

--- Fund_Analysis/Analysis_Class/file_list_sort.py
+++ Fund_Analysis/Analysis_Class/file_list_sort.py
@@ -18,15 +18,15 @@ for root, dirs, files in os.walk(root_folder):
         strategy = df['基金类型'].iloc[0]
         ticker = df['基金代码'].iloc[0]
         ticker = ticker.split('（')[0]
-        if strategy == '债券型-混合二级' or '债券型-长债' or '债券型-混合一级' or '债券型-中短债' or '指数型-固收' or 'QDII-纯债' or 'QDII-混合债':
+        if strategy in ['债券型-混合二级', '债券型-长债', '债券型-混合一级', '债券型-中短债', '指数型-固收', 'QDII-纯债', 'QDII-混合债']:
             bond_list.append(ticker)
 
-        elif strategy == '混合型-灵活' or '混合型-偏股' or '指数型-股票' or '股票型' or '混合型-偏债' or '混合型-绝对收益' or '混合型-平衡' or 'FOF-稳健型' or 'FOF-进取型' or 'FOF-均衡型':
+        elif strategy in ['混合型-灵活', '混合型-偏股', '指数型-股票', '股票型', '混合型-偏债', '混合型-绝对收益', '混合型-平衡', 'FOF-稳健型', 'FOF-进取型', 'FOF-均衡型']:
             stock_list.append(ticker)
 
-        elif strategy == '货币型-普通货币' or '货币型-浮动净值':
+        elif strategy in ['货币型-普通货币', '货币型-浮动净值']:
             money_market_list.append(ticker)
-        elif strategy == 'QDII-普通股票' or '指数型-海外股票' or '指数型-其他' or 'QDII-混合偏股' or 'QDII-混合灵活' or 'QDII-商品' or 'QDII-混合平衡' or 'QDII-REITs' or '商品' or 'QDII-FOF':
+        elif strategy in ['QDII-普通股票', '指数型-海外股票', '指数型-其他', 'QDII-混合偏股', 'QDII-混合灵活', 'QDII-商品', 'QDII-混合平衡', 'QDII-REITs', '商品', 'QDII-FOF']:
             overseas_list.append(ticker)
         else:
             others_list.append(ticker)
