b4d038ef2c8362b8fef8cfe39da5806b320482f3
freezingfire39 <yilunton@usc.edu>
2025-05-07 20:04:08 +0800

    Update test_3_bonds.py

--- Fund_Analysis/Analysis_Class/test_3_bonds.py
+++ Fund_Analysis/Analysis_Class/test_3_bonds.py
@@ -207,7 +207,7 @@ def main(symbol_file_path,symbol,search_file_path):
 
 
     index_comps = df_trade['accum_nav']
-    index_comps['comp_1'] = index_comps['comp_1'].fillna(method='ffill')
+    index_comps = index_comps.fillna(method='ffill')
 
     index_comps.index = pd.to_datetime(index_comps.index)
 
