d3cc4b61a031fba284c2a596aac97b85c4a2c1b6
freezingfire39 <yilunton@usc.edu>
2025-05-09 19:34:22 +0800

    Update test_2_money_market.py

--- Fund_Analysis/Analysis_Class/test_2_money_market.py
+++ Fund_Analysis/Analysis_Class/test_2_money_market.py
@@ -122,11 +122,11 @@ def main(symbol_file_path,symbol,search_file_path):
 
         close_days = df_test_1.index[-1]-df_test_2.index[-1]
 
-        df_target_2.at[df_target_2.index[-1],'purchase_days']  = "本基金距离上次开放赎回已经过去了"+int(close_days)+"天。"
+        df_target_2.at[df_target_2.index[-1],'purchase_days']  = "本基金距离上次开放赎回已经过去了"+str(close_days)+"天。"
         df_test_2 = df_test_2.to_frame()
         df_test_2['flag']=1
         df_test_5 = df_test_2['flag'].resample('Y').sum()
-        df_target_2.at[df_target_2.index[-1],'purchase_days_2']  = "本基金每年约有"+int(df_test_5.mean())+"天开放赎回"
+        df_target_2.at[df_target_2.index[-1],'purchase_days_2']  = "本基金每年约有"+str(df_test_5.mean())+"天开放赎回"
     
 
     df_target_2['CAGR'] = 0
