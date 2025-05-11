b9e8be3b29ea1de485bd3d2b3af6737be0504320
blesswenyeyu <blesssecret@outlook.com>
2025-05-10 18:58:59 -0700

    add test cases

--- server/test/testSuccess.py
+++ server/test/testSuccess.py
@@ -1,6 +1,5 @@
 import unittest
 import requests
-import json
 
 
 class APITestConfig:
@@ -21,20 +20,53 @@ class APITestConfig:
                 'amount': 1e6
             },
             'expected_status': 200,
-            'response_validation': lambda r: isinstance(r, dict)  # Simple validation function
+            'response_validation': lambda r: isinstance(r, dict)
+        },
+        {
+            'name': 'custom_portfolio_endpoint',
+            'method': 'POST',
+            'url': 'http://127.0.0.1:8000/api/v1/py/custom/portfolio/',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'data': {
+                "weights": [{"symbol": "000001", "weight": 0.1}, {"symbol": "000003", "weight": 0.9}],
+                "amount": 1e6
+            },
+            'expected_status': 200,
+            'response_validation': lambda r: isinstance(r, dict)
         },
         {
             'name': 'timing_endpoint',
-            'method': 'POST',
+            'method': 'GET',
             'url': 'http://127.0.0.1:8000/api/v1/py/timing/',
             'headers': {
                 'Content-Type': 'application/json'
             },
-            'data': {},  # Add your timing endpoint data here
+            # No data/params needed for this endpoint
             'expected_status': 200,
-            'response_validation': lambda r: isinstance(r, dict)
+            'response_validation': lambda response: (
+                isinstance(response, list) and  # Must be a list
+                len(response) > 0 and  # Not empty
+                all(isinstance(item, dict) for item in response) and  # All elements are dicts
+                all('trade_date' in item and isinstance(item['trade_date'], str) for item in response) and  # Has date field
+                all('close_sh' in item and isinstance(item['close_sh'], (int, float)) for item in response)  # Has numeric field
+            )
         }
-        # Add more test cases in the same format
+        ,
+        {
+            'name': 'default_portfolio_endpoint',
+            'method': 'GET',
+            'url': 'http://127.0.0.1:8000/api/v1/py/default/portfolio/',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            # No data/params needed for this endpoint
+            'expected_status': 200,
+            'response_validation': lambda response: (
+                    len(response) > 0   # Not empty
+            )
+        }
     ]
 
 
@@ -53,20 +85,26 @@ class TestAPI(unittest.TestCase):
         """
 
         def test_method(self):
+            # Prepare request arguments
+            kwargs = {
+                'method': test_case['method'],
+                'url': test_case['url'],
+                'headers': test_case.get('headers', {})
+            }
+
+            # Only add json/data for non-GET requests
+            if test_case['method'].upper() != 'GET' and 'data' in test_case:
+                kwargs['json'] = test_case['data']
+
             # Make the request
-            response = requests.request(
-                method=test_case['method'],
-                url=test_case['url'],
-                headers=test_case['headers'],
-                json=test_case['data']
-            )
+            response = requests.request(**kwargs)
 
             # Verify status code
             self.assertEqual(response.status_code, test_case['expected_status'],
                              f"Status code mismatch for {test_case['name']}")
 
             # Verify content type if specified
-            if 'Content-Type' in test_case['headers']:
+            if 'headers' in test_case and 'Content-Type' in test_case['headers']:
                 expected_content_type = test_case['headers']['Content-Type']
                 self.assertIn(expected_content_type, response.headers['Content-Type'],
                               f"Content-Type mismatch for {test_case['name']}")
